04a2e9d7d187da68a690240054df08be137dca44
KodFun <kodluyorumtv@gmail.com>
2023-06-01 22:35:28 +0300

    soru bg, css, font boyutu ayarlama

--- js/index.js
+++ js/index.js
@@ -11,11 +11,14 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      soru: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum!',
-      opt1: 'Lorem',
-      opt2: 'Ipsum',
+      soru: 'Sana mevcut veya muhtemel çocuğunun öğretmenini seçme fırsatı veriliyor. İşe başvuran öğretmenlerin özgeçmişlerinde eğitim ve becerilerinin yanı sıra isimlerinin, cinsiyetlerinin, milliyetlerinin ve bir fotoğraflarının yer almasını ister miydin?',
+      opt1: 'Evet',
+      opt2: 'Hayır',
       satirAdet: 0,
-      p5: null
+      p5: null,
+      img: null,
+      yaziBoyutYuzdesi: 48,
+      canvas: null
     };
   },
   mounted() {
@@ -23,32 +26,43 @@ createApp({
   },
   methods: {
     createSketch(p) {
-      p.setup = () => {
-        p.createCanvas(300, 300);
-        this.p5 = p;
-        // Your p5.js sketch setup code here
+      p.preload = () => {
+        this.img = p.loadImage('img/sorubg.png');
       };
-      
+
+      p.setup = () => {
+        this.canvas = p.createCanvas(1080, 1080);
+        this.p5 = p;
+        p.pixelDensity(1);
+      };
+
       p.draw = () => {
-        let fs = p.height * 0.044;
-        let lh = fs * 1.5;
+        p.image(this.img, 0, 0, p.width, p.height);
+
+        let fs = p.height * this.yaziBoyutYuzdesi / 1000; // fontsize
+        let lh = fs * 1.5; // line-height
+        let ml = p.width * 0.08; // margin-left
+        let cw = p.width - 2 * ml; // content width
+
 
         // Your p5.js sketch draw code here
-        p.background(149, 208, 255);
-        p.textSize(p.height * 0.044); // Set the font size
+        p.textSize(fs); // Set the font size
         p.textAlign(p.LEFT); // Set the text alignment to center
-        p.fill(0); // Set the text color to black
-        p.textFont("Playfair Display");
-        
+        p.fill(70, 66, 63); // text color
+        p.textFont("Roboto Condensed");
         p.textWrap(p.WORD);
         p.textLeading(lh); // Set the line-height
         let text = `${this.soru}
         
         A) ${this.opt1}
         B) ${this.opt2}`;
-        this.satirAdet = this.getLineCount(p, text, p.width*.8);
-        let y = p.height/2 - (lh * this.satirAdet)/2;
-        p.text(text, p.width/10, y, p.width*.8);
+        this.satirAdet = this.getLineCount(p, text, cw);
+        let y = p.height / 2 - (lh * this.satirAdet) / 2;
+        p.text(text, ml, y, cw);
+
+        p.textAlign(p.CENTER);
+        p.textSize(fs * 0.9);
+        p.text("@zorular", p.width * .50, p.height * .94);
       };
     },
     getLineCount(p, text, width) {
@@ -60,27 +74,28 @@ createApp({
         let line = lines[i];
         let words = line.split(' ');
         let xPos = 0, yPos = 0;
-      
+
         for (let i = 0; i < words.length; i++) {
           let word = words[i];
           let wordWidth = p.textWidth(word + ' ');
-      
+
           if (xPos + wordWidth > width) {
             lineCount++;
             xPos = 0;
             yPos += p.textLeading();
           }
-      
           xPos += wordWidth;
         }
-        
       }
-      
-    
       return lineCount;
     },
     kaydet() {
-      this.p5.saveCanvas('post', 'png');
+      this.p5.save("soru.png");
+    },
+    temizle() {
+      this.soru = "";
+      this.opt1 = "";
+      this.opt2 = "";
     }
   }
 }).mount('#app');
